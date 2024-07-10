f23b7e5d4660a85bf265ca75d6287b1f93ced4ca
Yender Reynoso <kokihidalgo18@gmail.com>
2024-07-10 10:59:19 -0400

    calculator 12

---
+++ 12.Calculator/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let display = document.querySelector(".display");
+const acBtn = document.querySelector("input.AC");
+const deleteBtn = document.querySelector(".DE");
+const decimalBtn = document.querySelector(".decimal");
+const divisionBtn = document.querySelector(".division");
+const multiBtn = document.querySelector(".multi");
+const minusBtn = document.querySelector(".minus");
+const addBtn = document.querySelector(".add");
+const equalBtn = document.querySelector(".equal");
+
+const nine = document.querySelector(".nine");
+const eight = document.querySelector(".eight");
+const seven = document.querySelector(".seven");
+const six = document.querySelector(".six");
+const five = document.querySelector(".five");
+const four = document.querySelector(".four");
+const three = document.querySelector(".three");
+const two = document.querySelector(".two");
+const one = document.querySelector(".one");
+const zero = document.querySelector(".zero");
+const zeroZero = document.querySelector(".zero-zero");
