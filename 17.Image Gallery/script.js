ecbf1bc068a6c08d6e41171a23f7181801662ad6
Yender Reynoso <kokihidalgo18@gmail.com>
2024-07-11 11:37:36 -0400

    Image Gallery 17

---
+++ 17.Image Gallery/script.js
@@ -0,0 +1,20 @@
+"use strict";
+const backBtn = document.getElementById("backBtn");
+const nextBtn = document.getElementById("nextBtn");
+let scrollContainer = document.querySelector(".gallery");
+
+scrollContainer.addEventListener("wheel", (e) => {
+  e.preventDefault();
+  scrollContainer.scrollLeft += e.deltaY;
+  scrollContainer.style.scrollBehavior = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
+});
+
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 900;
+});
