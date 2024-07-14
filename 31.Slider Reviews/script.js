234a7c71e057f3f419613d256f7ab985fefaf39f
Yender Reynoso <kokihidalgo18@gmail.com>
2024-07-14 17:06:21 -0400

    Slider Reviews

---
+++ 31.Slider Reviews/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const slide = document.getElementById("slide");
+const upArrow = document.getElementById("upArrow");
+const downArrow = document.getElementById("downArrow");
+
+let x = 0;
+let y = 0;
+
+upArrow.addEventListener("click", () => {
+  if (x > "-900") {
+    x = x - 300;
+    slide.style.top = x + "px";
+  }
+});
+
+downArrow.addEventListener("click", () => {
+  if (x < 0) {
+    x = x + 300;
+    slide.style.top = x + "px";
+  }
+});
